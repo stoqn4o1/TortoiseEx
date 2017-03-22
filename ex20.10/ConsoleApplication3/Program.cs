32069ca8914b119eae73b86718bbd476fa565505
stoqn4o1 <stoyanwws@abv.bg>
2017-03-22 15:26:08 +0200

    Deleted main method in a project...

--- ex20.10/ConsoleApplication3/Program.cs
+++ ex20.10/ConsoleApplication3/Program.cs
@@ -8,42 +8,6 @@ namespace ConsoleApplication3
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Random numbsGenerator = new Random();
-
-            int numb1 = numbsGenerator.Next(1,5);
-            int numb2 = numbsGenerator.Next(1,3);
-
-            Console.WriteLine($"What is {numb1} times {numb2}?");
-
-            int answer = int.Parse(Console.ReadLine());
-
-            if (answer == numb1*numb2)
-            {
-                int responce = numbsGenerator.Next(1, 4);
-                switch(responce)
-                {
-                    case 1:
-                        Console.WriteLine("Congrats!");
-                        break;
-                    case 2:
-                        Console.WriteLine("You're on fire!");
-                        break;
-                    default:
-                        Console.WriteLine("Correct!");
-                        break;
-                }
-
-            }
-            else
-            {
-                Console.WriteLine("You don't know shit...");
-            }
-
-            Console.ReadKey();
-
-
-        }
+       
     }
 }
